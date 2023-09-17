5278007346202a438034268079b80a9061bd0a58
suhail23599 <suhail23599@gmail.com>
2023-09-17 11:56:21 +0530

    commit

--- app.js
+++ app.js
@@ -5,27 +5,46 @@ import ReactDOM from 'react-dom/client'
 //many apps will have one root and one render method.
 // root is the place where React runs.
 // {} in createElement is to pass props / attributes like id, class.
-const heading = React.createElement('h1', {
-  id: 'title'
-}, 'Namaste all') // it prints an object
 const root = ReactDOM.createRoot(document.getElementById('root'))
-root.render(heading)
 // render method will override whatever is already existing inside root , as a fallback people write not rendered
 
 
 // Passing multiple child inside a container
-const heading1 = React.createElement('h1', {
-  id: 'title',
-  key: 'h1'
-}, 'Headin 1')
+// const heading = React.createElement('h1', {
+//   id: 'title',
+//   key: 'h1'
+// }, 'Heading')
 
-const heading2 = React.createElement('h2', {
-  id: 'title',
-  key: 'h2'
-}, 'heading 2')
+// const heading2 = React.createElement('h2', {
+//   id: 'title',
+//   key: 'h2'
+// }, 'heading 2')
+
+const heading = <h1 key="head">Namaste react</h1>  //JSX
+
+// JSX -> React.createElement -> Object -> HTML DOM
+// babel understands JSX and converts the code to React.createElement 
 
 const container = React.createElement('div', {
   id: 'container'
-}, [heading1, heading2])
+}, [heading])
 
-root.render(container) 
+const DummyComponent = () => (
+  <div>
+    DummyComponent
+  </div>
+)
+const HeaderComponent = () => {
+  return (
+    <div>
+      <h1>header component</h1>
+      { heading }
+      <DummyComponent>
+        
+      </DummyComponent>
+      { DummyComponent() }
+    </div>
+  )
+}
+
+root.render(<HeaderComponent></HeaderComponent>) 
