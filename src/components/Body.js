5e2926a37a871806ec15e05ca52b86b746b2ced3
suhail23599 <suhail23599@gmail.com>
2023-09-23 20:10:48 +0530

    episode-9

--- src/components/Body.js
+++ src/components/Body.js
@@ -1,16 +1,44 @@
 import { restaurantList } from '../constants'
 import RestaurantCard from './RestaurantCard'
-import { useState } from 'react'
+import { useState, useEffect } from 'react'
+import Shimmer from './Shimmer'
 
-const Body = () => { 
-  const [searchText, setSearchText] = useState('KFC') // returns [variable name, setVariable method]
-  const [restaurants, setRestaurants] = useState(restaurantList)
+const Body = () => {
+  // fetch -> Dont call Api HERE, because on every rerender it will call api 
+  const [searchText, setSearchText] = useState('') // returns [variable name, setVariable method]
+  const [restaurants, setRestaurants] = useState([])
+  const [filteredRestaurants, setFilteredRestaurants] = useState([])
+  // call back will be called only once if array is empty, it is called only when any of the depency satate variable changes 
+  useEffect(() => {
+  //  API
+  getRestaurants()
+  }, [])
 
-  function filterData (keyword, restaurants) {
-    const result = restaurants.filter(rest => rest.food.includes(keyword))
+  async function getRestaurants () {
+    const data = await fetch('https://www.swiggy.com/dapi/restaurants/list/v5?lat=12.9121181&lng=77.6445548&is-seo-homepage-enabled=true&page_type=DESKTOP_WEB_LISTING')
+    const json = await data.json()
+    const result = json.data?.cards[5].card.card.gridElements.infoWithStyle.restaurants
     setRestaurants(result)
+    setFilteredRestaurants(result)
   }
-  return (
+  function filterData (keyword) {
+    console.log(restaurants, 'res')
+    const result = restaurants.filter(rest => rest.info.name.includes(keyword))
+    setFilteredRestaurants(result)
+  }
+  if(!restaurants) {
+    return null // early return
+  }
+
+  if(filteredRestaurants.length === 0) {
+    return (
+      <>
+      <h1> No restaurant match </h1>
+      </>
+    )
+  }
+
+  return (restaurants.length === 0) ? <Shimmer></Shimmer> : (
     <>
       <div className='search-container'>
         <input
@@ -24,15 +52,15 @@ const Body = () => {
         </input>
         <button className='search-btn' onClick={() => {
           // filter data
-          filterData(searchText, restaurants)
+          filterData(searchText)
         }}>
           Search
         </button>
       </div>
       <div className="list">
         {
-          restaurants.map((rest, index) => {
-            return <RestaurantCard {...rest} key={index}></RestaurantCard>
+          filteredRestaurants.map((rest, index) => {
+            return <RestaurantCard {...rest.info} key={index}></RestaurantCard>
           })
         }
       </div>
--- src/components/RestaurantCard.js
+++ src/components/RestaurantCard.js
@@ -1,12 +1,13 @@
-const RestaurantCard = ({ imgUrl, food, cuisines, rating }) => {
+const RestaurantCard = ({ name, cuisines, avgRating }) => {
   return (
     <div className='card'>
-      <img src={imgUrl}></img>
-      <h2>{food}</h2>
-      <h3>{cuisines.join(',')}</h3>
-      <h4>{rating}</h4>
+      {/* <img src={imgUrl}></img> */}
+      <h2>{name}</h2>
+      <h3 className="word-break">{cuisines?.join(',')}</h3>
+      <h4>{avgRating}</h4>
     </div>
   )
 }
+
 
 export default RestaurantCard
